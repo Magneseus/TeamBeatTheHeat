ab16b2c6af82ecd443d8bc0e9c020a71f8fcb627
Matt Drouin <matthieudrouin@cmail.carleton.ca>
2017-10-28 18:51:30 -0400

    Comments

--- BeatTheStreet/app/src/main/java/com/beattheheat/beatthestreet/MainActivity.java
+++ BeatTheStreet/app/src/main/java/com/beattheheat/beatthestreet/MainActivity.java
@@ -190,7 +190,8 @@ public class MainActivity extends AppCompatActivity
 
         } else if (id == R.id.nav_view_stops) {
             // View all stops
-            // Starts a new activity that will display all stops saved from GTFS
+            /* Starts a new activity that will display all stops saved from GTFS
+               From there you can search for a stop and then go to a detailed stop view */
             Intent intent = new Intent(this, DisplayStopsActivity.class);
             startActivity(intent);
 
